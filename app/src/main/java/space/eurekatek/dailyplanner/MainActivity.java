d789805aaddfde8a456e3b230962a8ebae95d1f1
Domovoy13rus <zheka2001@list.ru>
2024-01-18 23:40:08 +0300

    edit file activity_main.xml, colors.xml, strings.xml, MainActivity.java; add files style_btn_stroke_black.xml

--- app/src/main/java/space/eurekatek/dailyplanner/MainActivity.java
+++ app/src/main/java/space/eurekatek/dailyplanner/MainActivity.java
@@ -3,6 +3,9 @@ package space.eurekatek.dailyplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        long date_start = calendar.get(Calendar.HOUR_OF_DAY);
+        long date_finish = calendar.get(Calendar.MINUTE);
+
+        String text = getString(R.string.time_task, date_start, date_finish);
+        TextView textView = new TextView(this);
+        //textView.setText(text);
+        //textView.setTextSize(28);
+        //setContentView(textView);
     }
 }
