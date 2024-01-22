d04232f5f0d47d7ec606255e3565c93f978cd409
Domovoy13rus <zheka2001@list.ru>
2024-01-22 23:00:09 +0300

    edit files activity_main.xml, about_task.xml, MainActivity.java, strings.xml

--- app/src/main/java/space/eurekatek/dailyplanner/MainActivity.java
+++ app/src/main/java/space/eurekatek/dailyplanner/MainActivity.java
@@ -2,9 +2,13 @@ package space.eurekatek.dailyplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,13 +17,29 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayoutTask = (LinearLayout)findViewById(R.id.linearLayoutTask);
+        linearLayoutTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, AboutTask.class);
+                    startActivity(intent); finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
 
         loadJson();
     }
@@ -35,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             String json;
             int max, id;
             String name, description;
-            Timestamp date_start, date_finish;
+            Date date_start, date_finish;
 
             json = new String(buffer, StandardCharsets.UTF_8);
 
@@ -45,18 +65,33 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < max; i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 id = jsonObject.getInt("id");
-                date_start = jsonObject.getJSONObject("date_start");
-                date_finish = jsonObject.getJSONObject("date_finish");
+                //date_start = jsonObject.getJSONObject("date_start");
+                //date_finish = jsonObject.getJSONObject("date_finish");
                 name = jsonObject.getString("name");
                 description = jsonObject.getString("description");
 
-                Log.e("TAG", "loadJson:  id: " + id + " date start: " + date_start + " date finish: " + date_finish + " name: " + name + " description: " + description);
-
+                Log.e("TAG", "loadJson:  id: " + id + " name: " + name + " description: " + description);
+                //Log.e("TAG", "loadJson:  id: " + id + " date start: " + date_start + " date finish: " + date_finish + " name: " + name + " description: " + description);
             }
         }
         catch (Exception e)
         {
             Log.e("TAG", "loadJson: error "+e);
         }
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (backPressedTime + 2000 > System.currentTimeMillis()){
+            super.onBackPressed();
+            return;
+        }else {
+            Toast.makeText(getBaseContext(), "Нажмите еще раз, чтобы выйти", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
     }
 }
